c871de8c0a319b6b9b2e18e62146be887904b006
emma <yaochen.jo@gmail.com>
2018-04-26 18:26:21 -0700

    add pbrt files

---
+++ build/src/ext/openexr/IlmBase/Imath/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/chenyao/Documents/GitHub/cs348b-2018-YaoChen/src/ext/openexr/IlmBase/Imath
+# Build directory: /Users/chenyao/Documents/GitHub/cs348b-2018-YaoChen/build/src/ext/openexr/IlmBase/Imath
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/src/ext/ptex/src/utils/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/chenyao/Documents/GitHub/cs348b-2018-YaoChen/src/ext/ptex/src/utils
+# Build directory: /Users/chenyao/Documents/GitHub/cs348b-2018-YaoChen/build/src/ext/ptex/src/utils
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
